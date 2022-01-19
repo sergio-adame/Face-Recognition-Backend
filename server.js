5f8ea284cdb5da8d1fdf58b9291e33dfb30ba89c
Sergio Adame <sergioadameva@gmail.com>
2022-01-19 00:58:51 -0600

    fixing res.send

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(cors())
 
 
-app.get('/', (req, res)=> { res.send(database.users) })
+app.get('/', (req, res)=> { res.send('it is working') })
 
 app.post ('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
